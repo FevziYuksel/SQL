8401675056d4f887e41322351235f2ee12b20198
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2022-08-30 23:42:05 +0300

    Refactor

--- HW4.sql
+++ HW4.sql
@@ -2,7 +2,7 @@
 SELECT DISTINCT replacement_cost FROM film;
 
 --2.film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
-SELECT DISTINCT COUNT(replacement_cost) FROM film;
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
 
 --3.film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 SELECT COUNT(*) FROM film
